a8fb4cfa7400718c6ad2a5ba61cf816c8b08aad9
Roshan1617 <roshan.16n@gmail.com>
2023-04-01 23:57:31 +0530

    hello

--- Basics_Maths/countDigits.java
+++ Basics_Maths/countDigits.java
@@ -2,15 +2,34 @@ package Basics_Maths;
 
 // import java.util.*;
 public class countDigits { 
-    static int count_digits(int n)
+    static int count_digits_1(int n)
     {
-        int digits = (int) Math.floor(Math.log10(n) + 1);
-        return digits;
+        int num = n;
+        int count = 0;
+        while(num != 0){
+            num /= 10;
+            count++;
+        }
+        return count;
     } 
+
+    static int count_digits_2(int n)
+    {
+        String str = Integer.toString(n);
+        return str.length();
+    } 
+
+    static int count_digits_3(int n)
+    {
+        int count = (int) Math.log10((int) n) + 1;
+        return count;
+    } 
+
+    
 
     public static void main(String args[]) 
     { 
-        int n = 12345;
-        System.out.println("Number of digits in "+n+" is "+count_digits(n));
+        int n = 100000;
+        System.out.println("Number of digits in "+n+" is "+count_digits_3(n));
     } 
 }
